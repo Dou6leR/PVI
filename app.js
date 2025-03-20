1155442edd62e4790b9b559baae1e15d7bea02de
Dou6leR <rostyslawoo@gmail.com>
2025-03-20 09:19:02 +0200

    PWA

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('../sw.js')
+        .then(registration => {
+            console.log('Service Worker registered:', registration);
+        })
+        .catch(error => {
+            console.log('Service Worker registration failed:', error);
+        });
+    });
+}
+
 function toggleBellAnimation() {
     const bell = document.querySelector('.bell_badge');
     const badge = document.querySelector('.badge');
---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "pwa-cache-v1";
+const ASSETS = [
+    "/PVI/students_page/students.html",
+    "/PVI/students_page/students.css",
+    "/PVI/students_page/students.js",
+    "/PVI/dashboard_page/dashboard.html",
+    "/PVI/dashboard_page/dashboard.css",
+    "/PVI/dashboard_page/dashboard.js",
+    "/PVI/messages_page/messages.html",
+    "/PVI/messages_page/messages.css",
+    "/PVI/messages_page/messages.js",
+    "/PVI/tasks_page/tasks.html",
+    "/PVI/tasks_page/tasks.css",
+    "/PVI/tasks_page/tasks.js",
+    "/PVI/index.html",
+    "/PVI/main.css",
+    "/PVI/app.js",
+    "/PVI/src/bell.png",
+    "/PVI/icons/user.png"
+];
+
+// Встановлення Service Worker та кешування файлів
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll(ASSETS);
+    })
+);
+});
+
+// Перехоплення запитів і завантаження з кешу
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+    caches.match(event.request).then((response) => {
+    return response || fetch(event.request);
+    })
+);
+});
+
+// Оновлення Service Worker і видалення старого кешу
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+    caches
+    .keys()
+    .then((keys) => {
+        return Promise.all(
+            keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        );
+    })
+    .then(() => {
+        return self.clients.claim(); // Підключаємо новий SW до всіх вкладок
+    })
+);
+});
